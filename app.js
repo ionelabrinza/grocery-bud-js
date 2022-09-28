5238c4fcf83bee61e3a71bdeae3a9565edf0d00e
ionelabrinza <ionela.brinza@yahoo.com>
2022-09-27 23:23:09 -0500

    function part 1

--- app.js
+++ app.js
@@ -9,11 +9,61 @@ const list = document.querySelector('.grocery-list')
 const clearBtn = document.querySelector('.clear-btn')
 
 // edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
 // ****** EVENT LISTENERS **********
+//submit form
+form.addEventListener('submit', addItem)
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
 
+    const id = new Date().getTime().toString()
+    if (value && !editFlag) {
+        const element = document.createElement('article');
+        //add class
+        element.classList.add('grocery-item');
+        //add id
+        const attr = document.createAttribute('data-id');
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = `<p class="title">${value}</p>
+        <div class="btn-container">
+            <button type="button" class="edit-btn">
+      <i class="fas fa-edit"></i>
+    </button>
+            <button type="button" class="delete-btn">
+      <i class="fas fa-trash"></i>
+    </button>
+        </div>`
+            //append child
+        list.appendChild(element);
+        //display alert
+        displayAlert('item added to the list', 'success');
+        //show container
+        container.classList.add('show-container')
+    } else if (value && editFlag) {
+
+    } else {
+        displayAlert('please enter value', 'danger')
+    }
+
+}
+// display alert
+function displayAlert(text, action) {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+    // remove alert
+    setTimeout(function() {
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`);
+    }, 1000)
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
